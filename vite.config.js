11745967cb007197f3fc1c2fda7d77888f5f9cc5
Rado <radoslav.minchev@gmail.com>
2025-04-04 01:28:24 +0300

    update index.html, add index.jsx, and include @fontsource/inter dependency

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true, // Automatically open the app in the browser
-  },
-  build: {
-    outDir: 'dist', // Output directory for the build
-  },
-});
+})
